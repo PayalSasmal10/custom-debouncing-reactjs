5f32bd19ecab676f419c59d32dba22910b22fdc9
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-06 20:54:25 +0530

    custom deboucing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import debounce from "lodash/debounce";
 
 function App() {
+  const [inputChange, setInputChange] = useState();
+
+  const sendRequest = useCallback((value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        // console.log(result);
+        return [result, value];
+      });
+  }, []);
+
+  const debounceSendRequest = useMemo(() => {
+    return debounce(sendRequest, 3000);
+  }, [sendRequest]);
+
+  const onChangeHandler = (e) => {
+    setInputChange(e.target.value);
+    // setTimeout(() => {
+    //   // apiFetching();
+    // }, 3000);
+    debounceSendRequest(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" name="search" onChange={onChangeHandler} />
     </div>
   );
 }
